8347baabef29fe5dba15c9994830cd5faf34ecab
Stephan Winter <wahren.testpin@online.de>
2025-04-22 21:13:23 +0200

    add: Logo

---
+++ client/f_splash.pas
@@ -0,0 +1,36 @@
+﻿unit f_splash;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Imaging.pngimage,
+  Vcl.ExtCtrls;
+
+type
+  TSplashform = class(TForm)
+    Image1: TImage;
+    Label1: TLabel;
+    Label2: TLabel;
+    Timer1: TTimer;
+    Label3: TLabel;
+    procedure Timer1Timer(Sender: TObject);
+  private
+    { Private-Deklarationen }
+  public
+    { Public-Deklarationen }
+  end;
+
+var
+  Splashform: TSplashform;
+
+implementation
+
+{$R *.dfm}
+
+procedure TSplashform.Timer1Timer(Sender: TObject);
+begin
+  Splashform.Free;
+end;
+
+end.
