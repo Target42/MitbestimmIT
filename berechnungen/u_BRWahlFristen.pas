e964583c388dde166cb63c36d1ac4f31d83ff559
Stephan Winter <wahren.testpin@online.de>
2025-04-22 21:14:07 +0200

    Wahlfristen und Kalender export

---
+++ berechnungen/u_BRWahlFristen.pas
@@ -0,0 +1,157 @@
+﻿{
+  This file is part of the MitbestimmIT project.
+
+  Copyright (C) 2025 Stephan Winter
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <https://www.gnu.org/licenses/>.
+}
+unit u_BRWahlFristen;
+
+interface
+
+uses
+  SysUtils, DateUtils;
+
+type
+  {
+    TWahlVerfahren:
+    Enumeration, die den Typ des Wahlverfahrens darstellt.
+
+    Werte:
+      - wvAllgemein: Stellt das allgemeine Wahlverfahren dar.
+      - wvVereinfacht: Stellt das vereinfachte Wahlverfahren dar.
+  }
+  TWahlVerfahren = (wvAllgemein, wvVereinfacht);
+
+  {
+    TWahlFristen ist ein Record, der verschiedene Termine und Fristen im Zusammenhang mit einem Wahlprozess darstellt.
+
+    Felder:
+      - Wahltag: TDate
+          Das Datum der Wahl.
+
+      - Verfahren: TWahlVerfahren
+          Der Typ oder das Verfahren der Wahl.
+
+      - SpaetesterWahlvorstand: TDate
+          Das späteste Datum, bis zu dem der Wahlvorstand gebildet sein muss.
+
+      - WahlausschreibenDatum: TDate
+          Das Datum, an dem die Wahlausschreibung erfolgt.
+
+      - VorschlagsfristEnde: TDate
+          Die Frist für die Einreichung von Vorschlägen oder Nominierungen.
+
+      - BekanntgabeVorschlaege: TDate
+          Das Datum, an dem die Vorschläge oder Nominierungen bekannt gegeben werden.
+
+      - BekanntgabeErgebnis: TDate
+          Das Datum, an dem die Wahlergebnisse bekannt gegeben werden.
+
+      - AnfechtungsfristEnde: TDate
+          Die Frist für die Anfechtung der Wahlergebnisse.
+  }
+  TWahlFristen = record
+    Wahltag: TDate;
+    Verfahren: TWahlVerfahren;
+
+    SpaetesterWahlvorstand: TDate;
+    WahlausschreibenDatum: TDate;
+    VorschlagsfristEnde: TDate;
+    BekanntgabeVorschlaege: TDate;
+    BekanntgabeErgebnis: TDate;
+    AnfechtungsfristEnde: TDate;
+  end;
+
+  {
+    TRegelwahlStatus:
+    Dieser Record repräsentiert den Status einer regulären Wahlperiode. Er enthält die folgenden Felder:
+
+    - InWahlperiode (Boolean): Gibt an, ob das aktuelle Datum innerhalb der Wahlperiode liegt.
+    - Jahr (Word): Das Jahr, das mit der Wahlperiode verbunden ist.
+    - Start (TDate): Das Startdatum der Wahlperiode.
+    - Ende (TDate): Das Enddatum der Wahlperiode.
+  }
+  TRegelwahlStatus = record
+    InWahlperiode: Boolean;
+    Jahr: Word;
+    Start: TDate;
+    Ende: TDate;
+  end;
+
+function PruefeRegulaereWahlperiode(Heute: TDate): TRegelwahlStatus;
+function BerechneWahlFristen(Wahltag: TDate; Verfahren: TWahlVerfahren): TWahlFristen;
+
+implementation
+
+{
+  /**
+   * Berechnet die Fristen für eine Wahl basierend auf dem Wahltag und dem Wahlverfahren.
+   *
+   * @param Wahltag Das Datum des Wahltags (TDate).
+   * @param Verfahren Das Wahlverfahren, das angewendet wird (TWahlVerfahren).
+   * @return Ein TWahlFristen-Objekt, das die berechneten Fristen enthält.
+   */
+}
+function BerechneWahlFristen(Wahltag: TDate; Verfahren: TWahlVerfahren): TWahlFristen;
+begin
+  Result.Wahltag := Wahltag;
+  Result.Verfahren := Verfahren;
+  Result.BekanntgabeErgebnis := Wahltag;
+  Result.AnfechtungsfristEnde := IncDay(Wahltag, 14);
+
+  if Verfahren = wvAllgemein then
+  begin
+    Result.WahlausschreibenDatum  := IncDay(Wahltag, -42); // 6 Wochen vor Wahl
+    Result.VorschlagsfristEnde    := IncDay(Result.WahlausschreibenDatum, 14); // 2 Wochen danach
+    Result.BekanntgabeVorschlaege := IncDay(Result.VorschlagsfristEnde, 1);
+    Result.SpaetesterWahlvorstand := IncDay(Wahltag, -70); // 10 Wochen vor Wahl
+  end
+  else // Vereinfachtes Verfahren
+  begin
+    Result.SpaetesterWahlvorstand := IncDay(Wahltag, -14); // mindestens 14 Tage vorher
+    Result.WahlausschreibenDatum  := IncDay(Wahltag, -7);   // spätestens 7 Tage vor Wahl
+    Result.VorschlagsfristEnde    := IncDay(Wahltag, -6);     // spätestens 6 Tage vor Wahl
+    Result.BekanntgabeVorschlaege := IncDay(Result.VorschlagsfristEnde, 1); // optional
+  end;
+end;
+
+{
+  /**
+   * Überprüft die reguläre Wahlperiode basierend auf dem angegebenen Datum.
+   *
+   * @param Heute Das aktuelle Datum, um die Wahlperiode zu bewerten.
+   * @return Ein Wert vom Typ TRegelwahlStatus, der den Status der regulären Wahlperiode angibt.
+   */
+}
+function PruefeRegulaereWahlperiode(Heute: TDate): TRegelwahlStatus;
+var
+  Jahr, Monat, Tag: Word;
+  WahlJahr: Word;
+begin
+  DecodeDate(Heute, Jahr, Monat, Tag);
+
+  // Berechne letztes reguläres Wahljahr (2022, 2026, 2030, …)
+  WahlJahr := 2022;
+  while WahlJahr + 4 <= Jahr do
+    Inc(WahlJahr, 4);
+
+  Result.Start := EncodeDate(WahlJahr, 3, 1);
+  Result.Ende  := EncodeDate(WahlJahr, 5, 31);
+  Result.Jahr  := WahlJahr;
+  Result.InWahlperiode := (Heute >= Result.Start) and (Heute <= Result.Ende);
+end;
+
+end.
+
---
+++ berechnungen/u_WahlfristenICS.pas
@@ -0,0 +1,120 @@
+﻿{
+  This file is part of the MitbestimmIT project.
+
+  Copyright (C) 2025 Stephan Winter
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <https://www.gnu.org/licenses/>.
+}
+unit u_WahlfristenICS;
+
+
+interface
+
+uses
+  SysUtils, Classes, u_BRWahlFristen;
+
+procedure ExportiereWahlfristenAlsICS(const Datei: string; const Fristen: TWahlFristen);
+
+implementation
+
+uses
+  System.DateUtils;
+
+{
+  Converts a given date to a string formatted as 'yyyymmdd' for use in ICS (iCalendar) files.
+
+  @param Datum The date to be converted.
+  @return A string representing the date in 'yyyymmdd' format.
+}
+function ICSDatum(Datum: TDate): string;
+begin
+  Result := FormatDateTime('yyyymmdd', Datum);
+end;
+
+{
+  Writes a line of text to a stream in ICS (iCalendar) format, appending a CRLF sequence.
+
+  @param F The stream to which the line will be written.
+  @param Zeile The string representing the line to be written to the stream.
+}
+procedure ICSZeile(const F: TStream; const Zeile: string);
+begin
+  F.Write(PAnsiChar(AnsiString(Zeile + #13#10))^, Length(Zeile) + 2);
+end;
+
+{
+  Creates an ICS calendar entry and writes it to the provided stream.
+
+  @param F     The stream to which the ICS calendar entry will be written.
+  @param Datum The date of the event.
+  @param Titel The title of the event, which will be used as both the summary and description.
+}
+procedure ICSKalendereintrag(const F: TStream; const Datum: TDate; const Titel: string);
+begin
+  ICSZeile(F, 'BEGIN:VEVENT');
+  ICSZeile(F, 'DTSTART;VALUE=DATE:' + ICSDatum(Datum));
+  ICSZeile(F, 'DTEND;VALUE=DATE:' + ICSDatum(IncDay(Datum, 1)));
+  ICSZeile(F, 'SUMMARY:' + Titel);
+  ICSZeile(F, 'DESCRIPTION:' + Titel);
+  ICSZeile(F, 'END:VEVENT');
+end;
+
+{
+  This procedure exports election deadlines (Wahlfristen) as an ICS (iCalendar) file.
+
+  @param Datei   The file path where the ICS file will be created.
+  @param Fristen A TWahlFristen object containing the election deadlines to be exported.
+
+  The procedure creates a new ICS file and writes calendar entries for the following events:
+  - Election day (Wahltag)
+  - Latest appointment of the election committee (Späteste Bestellung Wahlvorstand)
+  - Deadline for the election announcement (Wahlausschreiben spätestens)
+  - End of the proposal submission period (Ende Vorschlagsfrist)
+  - Announcement of valid proposals (Bekanntgabe gültiger Wahlvorschläge)
+  - Announcement of election results (Bekanntgabe Wahlergebnis)
+  - End of the contestation period (Fristende Wahlanfechtung)
+
+  The ICS file is structured according to the iCalendar format, starting with "BEGIN:VCALENDAR"
+  and ending with "END:VCALENDAR". Each event is added as a calendar entry using the
+  ICSKalendereintrag helper procedure.
+
+  The file is created using a TFileStream with fmCreate mode, ensuring that any existing file
+  at the specified path will be overwritten. The TFileStream is properly freed after use.
+}
+procedure ExportiereWahlfristenAlsICS(const Datei: string; const Fristen: TWahlFristen);
+var
+  F: TFileStream;
+begin
+  F := TFileStream.Create(Datei, fmCreate);
+  try
+    ICSZeile(F, 'BEGIN:VCALENDAR');
+    ICSZeile(F, 'VERSION:2.0');
+    ICSZeile(F, 'PRODID:-//MitbestimmIT//DE');
+
+    ICSKalendereintrag(F, Fristen.Wahltag, 'Wahltag');
+    ICSKalendereintrag(F, Fristen.SpaetesterWahlvorstand, 'Späteste Bestellung Wahlvorstand');
+    ICSKalendereintrag(F, Fristen.WahlausschreibenDatum, 'Wahlausschreiben spätestens');
+    ICSKalendereintrag(F, Fristen.VorschlagsfristEnde, 'Ende Vorschlagsfrist');
+    ICSKalendereintrag(F, Fristen.BekanntgabeVorschlaege, 'Bekanntgabe gültiger Wahlvorschläge');
+    ICSKalendereintrag(F, Fristen.BekanntgabeErgebnis, 'Bekanntgabe Wahlergebnis');
+    ICSKalendereintrag(F, Fristen.AnfechtungsfristEnde, 'Fristende Wahlanfechtung');
+
+    ICSZeile(F, 'END:VCALENDAR');
+  finally
+    F.Free;
+  end;
+end;
+
+end.
+
