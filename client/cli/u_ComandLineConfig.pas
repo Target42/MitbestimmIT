f08a148eb059c2ec2c8bfb5e87a175057bf77cea
Stephan Winter <wahren.testpin@online.de>
2025-05-11 16:27:52 +0200

    comandline parameter: host, user, passwort

---
+++ client/cli/u_ComandLineConfig.pas
@@ -0,0 +1,33 @@
+﻿unit u_ComandLineConfig;
+
+interface
+
+
+implementation
+
+uses
+  VSoft.CommandLine.Options,
+  u_ComandOptions;
+
+procedure ConfigureOptions;
+var
+  option : IOptionDefinition;
+  cmd    : TCommandDefinition;
+begin
+
+  TOptionsRegistry.NameValueSeparator := '=';
+
+  option := TOptionsRegistry.RegisterOption<string>('host','h','the host to connect',
+    procedure(const value : string)
+    begin
+        THostOptions.Host := value;
+    end);
+
+end;
+
+
+
+initialization
+  ConfigureOptions;
+
+end.
---
+++ client/cli/u_ComandOptions.pas
@@ -0,0 +1,21 @@
+﻿unit u_ComandOptions;
+
+interface
+
+uses
+   VSoft.CommandLine.Parser;
+
+type
+  THostOptions =  class
+  public
+    class var
+      Host : string;
+    class var
+      User : string;
+    class var
+      PWd  : string;
+  end;
+
+implementation
+
+end.
